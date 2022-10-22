d52c0a737484353e7d93728f1d8139405bacff67
huynhnguyen-haku <huynhnguyen1503@gmail.com>
2022-10-22 21:41:33 +0700

    First Commit

--- 2022_Semester5_Fall/PRN211/Demo/AutomobileSolution_Lab2/AutomobileLibrary/BusinessObject/Car.cs
+++ 2022_Semester5_Fall/PRN211/Demo/AutomobileSolution_Lab2/AutomobileLibrary/BusinessObject/Car.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace AutomobileLibrary.BusinessObject
 {
-    internal class Car
+    public class Car
     {
+        public int CarID { get; set; }
+        public string CarName { get; set; }
+        public string Manufacturer { get; set; }
+        public decimal Price { get; set; }
+        public int ReleaseYear { get; set; }
+
     }
 }
