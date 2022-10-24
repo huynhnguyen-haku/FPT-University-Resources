61b935dd3cafd7365a05e21e4f745c6144867380
huynhnguyen-haku <huynhnguyen1503@gmail.com>
2022-10-24 15:23:25 +0700

    Add more Codes

--- 2022_Semester5_Fall/PRN211/Demo/AutomobileSolution_Lab2/AutomobileLibrary/DataAccess/BaseDAL.cs
+++ 2022_Semester5_Fall/PRN211/Demo/AutomobileSolution_Lab2/AutomobileLibrary/DataAccess/BaseDAL.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.IO;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace AutomobileLibrary.DataAccess
 {
-    internal class BaseDAL
+    public class BaseDAL
     {
+        public StockDataProvider dataProvider { get; set; } = null;
+        public SqlConnection connection = null;
+        //--------------------------------
+        public BaseDAL()
+        {
+            var connectionString = GetConnectionString();
+            dataProvider = new StockDataProvider(connectionString);
+        }
+        //--------------------------------
+        public string GetConnectionString()
+        {
+            string connectionString;
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettng.json, true, true")
+                .Build();
+            connectionString = config["ConnectiongString:MyStockDB"];
+            return connectionString;
+        }
+        //--------------------------------
+        public void CloseConnection()
+        {
+            dataProvider.CloseConnection(connection);
+        }
     }
 }
--- 2022_Semester5_Fall/PRN211/Demo/AutomobileSolution_Lab2/AutomobileLibrary/Repository/ICarRepository.cs
+++ 2022_Semester5_Fall/PRN211/Demo/AutomobileSolution_Lab2/AutomobileLibrary/Repository/ICarRepository.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using AutomobileLibrary.BusinessObject;
 
 namespace AutomobileLibrary.Repository
 {
-    internal class ICarRepository
+    public interface ICarRepository
     {
+        IEnumerable<Car> GetCars();
+        Car GetCarByID(int carId);
+        void InsertCar(Car car);
+        void DeleteCar(int carId);
+        void UpdateCar(Car car);
     }
 }
